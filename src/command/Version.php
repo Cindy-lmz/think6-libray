6d8264b29fccb12c3be29f93e4c2102b20b88c4e
Cindy-lmz <1481380328@qq.com>
2020-10-27 12:14:08 +0800

    初始化

---
+++ src/command/Version.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace think\admin\command;
+
+use think\admin\Command;
+use think\console\Input;
+use think\console\Output;
+
+/**
+ * 框架版本号指令
+ * Class Version
+ * @package think\admin\command
+ */
+class Version extends Command
+{
+    protected function configure()
+    {
+        $this->setName('xadmin:version');
+        $this->setDescription("ThinkLibrary and ThinkPHP Version");
+    }
+
+    /**
+     * @param Input $input
+     * @param Output $output
+     */
+    protected function execute(Input $input, Output $output)
+    {
+        $output->writeln('ThinkLib ' . $this->process->version());
+        $output->writeln('ThinkPHP ' . $this->app->version());
+    }
+}
