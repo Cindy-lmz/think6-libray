09f94e9df324fab2caa308f303da15fdd97e235f
cindy-lmz <1481380328@qq.com>
2020-10-27 19:17:29 +0800

    no message

--- src/helper/FormHelper.php
+++ src/helper/FormHelper.php
@@ -39,7 +39,7 @@ class FormHelper extends Helper
             }
             if (false !== $this->class->callback('_form_filter', $data)) {
                 // $this->class->fetch($template, ['vo' => $data]);
-                $this->class->success($template,['vo' => $data],200);
+                $this->controller->success($template.'信息请求成功',['vo' => $data]);
             } else {
                 return $data;
             }
@@ -49,9 +49,9 @@ class FormHelper extends Helper
                 $result = data_save($query, $data, $field, $where) !== false;
                 if (false !== $this->class->callback('_form_result', $result, $data)) {
                     if ($result !== false) {
-                        $this->class->success(lang('think_library_form_success'));
+                        $this->controller->success('成功','');
                     } else {
-                        $this->class->error(lang('think_library_form_error'));
+                        $this->controller->error('失败','',1010);
                     }
                 }
                 return $result;
